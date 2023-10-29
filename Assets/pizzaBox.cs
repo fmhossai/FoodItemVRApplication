996d86bbadb881576041c32fbd5d0c66f24c6392
Abid-Al-Labib <getabidallabib@gmail.com>
2023-10-29 02:30:05 -0600

    updating with lab progress

---
+++ Assets/pizzaBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class pizzaBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver()
+    {
+        GameObject obj = GameObject.Find("Summary");
+        TMP_Text txt = obj.GetComponent<TMP_Text>();
+        txt.text = "hello";
+
+    }
+}
