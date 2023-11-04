37166f835b381cef7aca6e50b0b75a9a104afcad
Abid-Al-Labib <getabidallabib@gmail.com>
2023-11-03 19:26:06 -0600

    stereoscopy changes from lab

---
+++ Assets/placeOrder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placeOrder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseOver()
+    {
+        GetComponent<Renderer>().material.color = new Color(0.9f, 0.8f, 0.6f, 1f);
+    }
+
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
